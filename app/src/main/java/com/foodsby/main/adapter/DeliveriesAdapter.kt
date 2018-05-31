e9f8054208b47230c45d551ca1ca2fd3f67ec643
Joe <jroskopf14@alumni.uwosh.edu>
2018-05-30 20:25:47 -0500

    adapter styling

--- app/src/main/java/com/foodsby/main/adapter/DeliveriesAdapter.kt
+++ app/src/main/java/com/foodsby/main/adapter/DeliveriesAdapter.kt
@@ -2,6 +2,8 @@ package com.foodsby.main.adapter
 
 import android.content.Context
 import android.net.Uri
+import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -17,6 +19,8 @@ import kotlinx.android.synthetic.main.delivery_row_item.view.*
 
 class DeliveriesAdapter(private var deliveries: List<Delivery>, private val context: Context) : RecyclerView.Adapter<ViewHolder>() {
 
+    private var expandedPosition = -1
+    private var previousExpandedPosition  = -1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(context).inflate(R.layout.delivery_row_item, parent, false))
@@ -30,14 +34,79 @@ class DeliveriesAdapter(private var deliveries: List<Delivery>, private val cont
         holder.deliveryItemDeliveryTime.text = dropoff.dropoff
         holder.deliveryItemRestaurantImage.setImageURI(Uri.parse(dropoff.logoUrl))
 
-        when {
-            dropoff.isPastCutoff == true -> holder.deliveryItemOrderStatus.text = "Cut-Off Time Passed"
-            dropoff.isOrderPlaced == true -> holder.deliveryItemOrderStatus.text = "Order is placed!"
-            dropoff.soldOut == true -> holder.deliveryItemOrderStatus.text = "Order is sold out!"
-            dropoff.sellingOut == true -> holder.deliveryItemOrderStatus.text = "Order is selling out, hurry!"
+        holder.deliveryItemStatusImage.setImageDrawable(ContextCompat.getDrawable(context, getImageFromDropOff(dropoff)))
+
+        val isExpanded = position == expandedPosition
+        holder.deliveryItemDeliveryStatus.visibility = if (isExpanded) View.VISIBLE else View.GONE
+
+        if(canBeExpanded(dropoff)) {
+            holder.deliveryItemDeliveryStatus.text = getStatusFromDropOff(dropoff)
+            holder.deliveryItemDeliveryStatus.setBackgroundColor(ContextCompat.getColor(context, getBackgroundColorFromDropOff(dropoff)))
+
+            holder.itemView.isActivated = isExpanded
+
+            if (isExpanded) {
+                previousExpandedPosition = position
+            }
+
+            holder.itemView.setOnClickListener {
+                expandedPosition = if (isExpanded) -1 else position
+                notifyItemChanged(previousExpandedPosition)
+                notifyItemChanged(position)
+            }
+        } else {
+            holder.itemView.setOnClickListener {
+                Snackbar.make(it, "Order placed!", Snackbar.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun canBeExpanded(dropOff: Delivery): Boolean {
+        return when {
+            dropOff.soldOut == true -> true
+            dropOff.isOrderPlaced == true -> true
+            dropOff.isPastCutoff == true -> true
+            dropOff.sellingOut == true -> true
+            else -> {
+                false
+            }
+        }
+    }
+
+    private fun getBackgroundColorFromDropOff(dropOff: Delivery): Int {
+        return when {
+            dropOff.soldOut == true -> R.color.material_red_500
+            dropOff.isOrderPlaced == true -> R.color.material_green_500
+            dropOff.isPastCutoff == true -> R.color.material_red_500
+            dropOff.sellingOut == true -> R.color.material_yellow_500
+            else -> {
+                android.R.color.white
+            }
+        }
+    }
 
 
+    private fun getImageFromDropOff(dropOff: Delivery): Int {
+        return when {
+            dropOff.soldOut == true -> R.drawable.ic_sentiment_dissatisfied_red_500_24dp
+            dropOff.isOrderPlaced == true -> R.drawable.ic_check_circle_green_500_24dp
+            dropOff.isPastCutoff == true -> R.drawable.ic_timer_off_red_500_24dp
+            dropOff.sellingOut == true -> R.drawable.ic_warning_yellow_500_24dp
+            else -> {
+                android.R.drawable.screen_background_dark_transparent
+            }
+        }
+    }
+
+
+    private fun getStatusFromDropOff(dropoff: Delivery): String {
+        return when {
+            dropoff.isPastCutoff == true -> "Cut-Off Time Passed"
+            dropoff.isOrderPlaced == true -> "Order is placed!"
+            dropoff.soldOut == true ->"Order is sold out!"
+            dropoff.sellingOut == true -> "Order is selling out, hurry!"
+            else -> "Place your order!"
+        }
     }
 
     // Gets the number of deliveries in the list
@@ -46,6 +115,9 @@ class DeliveriesAdapter(private var deliveries: List<Delivery>, private val cont
     }
 
     fun update(newDeliveries: List<Delivery>) {
+        expandedPosition = -1
+        previousExpandedPosition = -1
+
         val diffCallback = DiffUtilCallback(newDeliveries, deliveries)
         val diffResult = DiffUtil.calculateDiff(diffCallback, true)
 
@@ -58,7 +130,8 @@ class DeliveriesAdapter(private var deliveries: List<Delivery>, private val cont
 class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val deliveryItemRestaurantName = view.deliveryItemRestaurantName as TextView
     val deliveryItemRestaurantImage = view.deliveryItemRestaurantImage as ImageView
+    val deliveryItemStatusImage = view.deliveryItemStatusImage as ImageView
     val deliveryItemOrderByTime = view.deliveryItemOrderByTime as TextView
     val deliveryItemDeliveryTime = view.deliveryItemDeliveryTime as TextView
-    val deliveryItemOrderStatus = view.deliveryItemOrderStatus as TextView
+    val deliveryItemDeliveryStatus = view.deliveryItemDeliveryStatus as TextView
 }
